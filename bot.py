589d334f3d2898a1760cab126b4b0cde3983427b
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2022-03-24 23:35:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -192,7 +192,54 @@ async def otoreklamm(event):
         await bot.send_message("me", "Gönderildi !")
         await asyncio.sleep(14500)
 # ----------------------------------------------------------------------------------
+idler =  [
+    -1001275989066,
+    -1001435523233,
+    -1001276667828,
+    -1001399376603,
+    -1001377871517,   
+    -1001254179689,
+    -1001226168546,
+    -1001245244239,   
+    -1001224401851,
+    -1001561556131, 
+    -1001535512195,
+    -1001421722479,
+    -1001653411802,
+    -1001172663751
+]
 
+@register(outgoing=True, pattern="^.gonder")
+async def deneme(event):
+    global idler
+    reply = await event.get_reply_message()
+    hata = 0
+    m = await event.respond(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : `Gönderiliyor...`")
+    for x in idler:
+        try:
+            await reply.forward_to(x)
+        except Exception as e:
+            hata += 1
+            await event.respond(str(e))
+    await m.edit(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------------------------------------------
 @bot.on(events.NewMessage(pattern="^.otodata"))
 async def otoreklamm(event):
     global id_list2
